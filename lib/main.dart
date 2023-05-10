71c0fb48bbedd4af2a9c8c9f057d12e5111398a8
Jaiun Lee <titusjulee@outlook.com>
2023-05-10 22:19:55 +0900

    UPDATE FIRST BATCH, TEST-FIELD 20230510 2220 LOCAL DEV ENV

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,19 @@ void main() async {
   final appState = FFAppState(); // Initialize FFAppState
   await appState.initializePersistedState();
 
+  //20230510 : ADDED BY JAIUN BELOW :: ADD NATIVE APP KEYS//
+
+  // 웹 환경에서 카카오 로그인을 정상적으로 완료하려면 runApp() 호출 전 아래 메서드 호출 필요
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // runApp() 호출 전 Flutter SDK 초기화
+  KakaoSdk.init(
+      nativeAppKey: '${YOUR_NATIVE_APP_KEY}',
+      javaScriptAppKey: '${YOUR_JAVASCRIPT_APP_KEY}',
+  );
+
+  //20230510 : ADDED BY JAIUN ABOVE :: ADD NATIVE APP KEYS//
+
   runApp(ChangeNotifierProvider(
     create: (context) => appState,
     child: MyApp(),
